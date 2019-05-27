f7254ce7251b11aeecac8bc30463d622b99d3d11
Clemilton Pereira <clemilton.ufam@gmail.com>
2019-05-27 08:32:16 -0400

    Adicionar produtos por um JSON

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //variables
+
+//PAREI EM 1h:29min
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
@@ -17,9 +19,16 @@ class Products{
     //método assicrono para pegar os dados
     async getProducts(){
         try{
-        let result = await fetch('products.json')
-        let data = await result.json()
-        return data
+            let result = await fetch('products.json')
+            let data = await result.json()
+            let products =  data.items;
+            products = products.map(item => {
+                const{title,price} = item.fields
+                const{id} = item.sys
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+            });
+            return products;
         }catch(error){
             console.log(error)
         }
@@ -28,7 +37,30 @@ class Products{
 
 // display products
 class UI{
-
+    displayProducts(products){
+        console.log(products)
+        let result ='';
+        products.forEach(product => {
+            result+=`
+            <!--sigle product-->
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                    <i class="fas fa-shopping-cart"></i>
+                    add to bag
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>${product.price}</h4>
+            </article>
+            <!--end of single product-->
+            
+            
+            `
+        });
+        productsDOM.innerHTML= result;
+    }
 }
 
 //local storage
@@ -39,7 +71,7 @@ class Storage{
 document.addEventListener("DOMContentLoaded",()=>{
     const ui = new UI();
     const products = new Products();
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => ui.displayProducts(products));
 });
 
 
